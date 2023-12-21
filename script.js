8570c0e5f9cc4a089149591ad65a29998b2e5e07
zabed-iftekhar <144038095+Zabed-Iftekhar@users.noreply.github.com>
2023-12-21 11:42:42 +0600

    Create two functions getComputerChoice() and getPlayerChoice()

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-//
+//script.js//
+
+
+function getComputerChoice(){
+    let choices=["rock","paper","scissors"];
+    let randomChoice=choices[Math.floor(Math.random()*3)];
+    let computerSelection=randomChoice;
+    return computerSelection;
+    
+}
+
+
+function getPlayerChoice(){
+    let playerSelection=prompt("What choice would you make in this round ?","");
+    playerSelection=playerSelection.toLowerCase();
+    if (!(playerSelection==="rock" || playerSelection=="paper"||
+    playerSelection=="scissors")){
+        console.log("incorrect input");
+    }
+    else{
+        return playerSelection;
+    }
+        
+}
+
+
+let computerSelection=getComputerChoice();
+let playerSelection=getPlayerChoice();
+console.log(`Computer: ${computerSelection}`);
+console.log(`You: ${playerSelection}`);
