c1e31146521078d4a8aeced2b4d0441bf9d1e161
zabed-iftekhar <144038095+Zabed-Iftekhar@users.noreply.github.com>
2024-01-08 12:37:53 +0600

    Remove game() and add game logic to playRound() function.
    -Implemented the game logic inside the playRound() function to reduce complexity. 
    - Learnt DOM manipulation and used it to return the score and winner at the end of 5 rounds.

--- script.js
+++ script.js
@@ -1,116 +1,109 @@
 //script.js//
 
+let btnRock=document.querySelector('#rock');
+let btnPaper=document.querySelector('#paper');
+let btnScissors=document.querySelector('#scissors');
+let playerScoreDiv=document.querySelector('#player-score');
+let computerScoreDiv=document.querySelector('#computer-score');
+let result =document.querySelector('.result');
+
+let playerSelection="";
+let computerSelection="";
+let playerScore=0;
+let computerScore=0;
+
+playerScoreDiv.append(playerScore);
+computerScoreDiv.append(computerScore);
+
+btnPaper.addEventListener('click',()=>{
+    playerSelection=btnPaper.innerText;
+    computerSelection=getComputerChoice();
+    playRound(playerSelection,computerSelection);
+    
+    
+})
+    
+
+btnScissors.addEventListener('click',()=>{
+    playerSelection=btnScissors.innerText;
+    computerSelection=getComputerChoice();
+    playRound(playerSelection,computerSelection);
+    
+})
+
+
+ btnRock.addEventListener('click',function rockFunction(){
+
+    playerSelection=btnRock.innerText;
+    computerSelection=getComputerChoice();
+    playRound(playerSelection,computerSelection);
+    
+    
+})
+
+
 
 function getComputerChoice(){
-    let choices=["rock","paper","scissors"];
+    let choices=["Rock","Paper","Scissors"];
     let randomChoice=choices[Math.floor(Math.random()*3)];
-    let computerSelection=randomChoice;
+    computerSelection=randomChoice;
     return computerSelection;
-    
-}
-
-
-function getPlayerChoice(){
-    let playerSelection=prompt("What choice would you make in this round ?","");
-    playerSelection=playerSelection.toLowerCase();
-    if (!(playerSelection==="rock" || playerSelection=="paper"||
-    playerSelection=="scissors")){
-        console.log("incorrect input");
-    }
-    else{
-        return playerSelection;
-    }    
-    
-}
-
-
-
-let winner=""
-
-game();
-
-function game(){
-
-    //this function calls playRound() 5 times.
-
-    let result=0;
-
-    while(result<5){
-        
-        let computerSelection=getComputerChoice();
-        let playerSelection=getPlayerChoice();
-        
-        
-        console.log(`Computer: ${computerSelection}`);
-        console.log(`You: ${playerSelection}`);
-        result++
-        console.log(`Round ${result} result is : `);
-        playRound(computerSelection,playerSelection);
-       
-   
-}
 }
 
 
 
 
-
-
-
-function playRound(computerSelection,playerSelection){
+function playRound(playerSelection,computerSelection){
     
-    //this function logs if the player has won or the computer has won based on the playerSelection and computerSelection
     
-    if (playerSelection==="rock" && computerSelection==="scissors"){
-        console.log(`Computer chose ${computerSelection} and you chose ${playerSelection}. Rock breaks scissors.
-        You win !`)     
-        
-
-    }
-
-    
-    else if (playerSelection==="scissors" && computerSelection==="rock"){
-        console.log(`Computer chose ${computerSelection} and you chose ${playerSelection}.
-        Rock breaks scissors.You lose.`)
-        
-    
-    }
-    
-    else if (playerSelection==="paper" && computerSelection==="scissors"){
-        console.log(`Computer chose ${computerSelection} and you chose ${playerSelection}.
-        Scissors cut paper.You lose.`)
+    if (playerSelection==computerSelection){
+        result.innerText=`Computer chose ${playerSelection}. It's a tie. Try again !`
         
     }
 
-    else if (playerSelection==="scissors" && computerSelection==="paper"){
-        console.log(`Computer chose ${computerSelection} and you chose ${playerSelection}.
-        Scissors cut paper.You win!`)
+    else if (computerSelection=="Paper" && playerSelection==="Rock" ||
+    computerSelection=="Scissors" && playerSelection==="Paper" ||
+    computerSelection=="Rock" && playerSelection==="Scissors" ){
+        result.innerText=`Computer chose ${computerSelection}. You lose.`
+        computerScore=++computerScore;
+        computerScoreDiv.innerText=`Computer Scored :${computerScore}`;
         
-          
-       
-    }
-    
-    else if (playerSelection==="paper" && computerSelection==="rock"){
-        console.log(`Computer chose ${computerSelection} and you chose ${playerSelection}.
-        Paper wraps rock.You win!`)
-       
+        console.log(`Computer scored ${computerScore}`);
+        if (computerScore===5){
+            result.innerText="Computer scored 5, you lost the game. Do you want to play again ?";
+            computerScore=0;
+            playerScore=0;
+        }
+        else if (playerScore===5){
+            result.innerText="You scored 5, you win the game ! Play again !";
+            computerScore=0;
+            playerScore=0;
+        }
         
-        
-    }
-    else if (playerSelection==="rock" && computerSelection==="paper"){
-        console.log(`Computer chose ${computerSelection} and you chose ${playerSelection}.
-        Paper wraps rock.You lose.`)
-         
-        
-    }
-            
-            
 
-    else if (playerSelection===computerSelection){
-        console.log(`Computer chose ${computerSelection} and you chose ${playerSelection}. It\'s a tie ! Try again.`)
-        
     }
-    
+    else if (computerSelection=="Rock" && playerSelection==="Paper" ||
+    computerSelection=="Paper" && playerSelection==="Scissors" ||
+    computerSelection=="Scissors" && playerSelection==="Rock" ){
+        result.innerText=`Computer chose ${computerSelection}. You win !`
+        
+         playerScore=++playerScore;
+         playerScoreDiv.innerText=`You Scored :${playerScore}`;
 
-    
+        console.log(`You scored ${playerScore}`);
+        if (playerScore===5){
+            result.innerText="You scored 5, you win ! Play again !";
+            computerScore=0;
+            playerScore=0;
+        }
+        else if (computerScore===5){
+            result.innerText="Computer scored 5, you lost the game. Do you want to play again ?";
+            computerScore=0;
+            playerScore=0;
+        }
+        
+    }  
 }
+
+
+
